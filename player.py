13c348d97c26d0def080013b4e61a4052140da01
Mkupr <mkupr@outlook.com>
2023-07-21 20:56:47 +0300

    fix player to field

--- player.py
+++ player.py
@@ -17,12 +17,23 @@ class Player:
         # если нажата клавиша влево, переместить игрока влево
         if keys[pygame.K_LEFT]:
             self.rect.x -= self.speed
+            if self.rect.x < 0:
+                self.rect.x = 800
+
         # если нажата клавиша вправо, переместить игрока вправо
         if keys[pygame.K_RIGHT]:
             self.rect.x += self.speed
+            if self.rect.x > 800:
+                self.rect.x = 0
+
         # если нажата клавиша вверх, переместить игрока вверх
         if keys[pygame.K_UP]:
             self.rect.y -= self.speed
+            if self.rect.y < 0:
+                self.rect.y = 600
         # если нажата клавиша вниз, переместить игрока вниз
         if keys[pygame.K_DOWN]:
             self.rect.y += self.speed
+            if self.rect.y > 600:
+                self.rect.y = 0
+        print(self.rect.center)
